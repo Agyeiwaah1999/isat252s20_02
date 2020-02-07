a4ec4cc0d9eb7ec698d72761df0b1db463d988e0
vida <59853574+Agyeiwaah1999@users.noreply.github.com>
2020-02-07 12:32:23 -0500

    Add files via upload

---
+++ Banboo program.py
@@ -0,0 +1,9 @@
+for x in range(100):
+    if x%16 == 0 and x !=0:
+        print("banboo")
+    elif x%8 == 0 and x !=0:
+        print("boo")
+    elif x%2 == 0 and x !=0:
+        print("ban")
+    else:
+        print(x)
